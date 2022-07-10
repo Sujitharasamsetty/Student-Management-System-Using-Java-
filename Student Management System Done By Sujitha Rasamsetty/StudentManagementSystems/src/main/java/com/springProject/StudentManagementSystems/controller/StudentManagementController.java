7295f5b25ee8617ac6c6b89035fff6accf4022f4
SUJITHA RASAMSETTY <73248098+Sujitharasamsetty@users.noreply.github.com>
2022-07-10 19:37:46 +0530

    Add files via upload

---
+++ Student Management System Done By Sujitha Rasamsetty/StudentManagementSystems/src/main/java/com/springProject/StudentManagementSystems/controller/StudentManagementController.java
@@ -0,0 +1,98 @@
+package com.springProject.StudentManagementSystems.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springProject.StudentManagementSystems.model.StudentRecord;
+import com.springProject.StudentManagementSystems.service.StudentManagementService;
+/*
+ * Teacher records 
+ * Teachers(id,name,contact_no)
+ * id,name,contactNo
+ * 
+ * get teacherRecord/{id}
+ * get techerRecords
+ * put techerRecord/add
+ * post techerRecord/edit/{id}
+ * delete techerRecord/delete/{id} 
+ * 
+ */
+@RestController
+public class StudentManagementController {
+
+	@Autowired
+	private StudentManagementService studentManagementService;
+	
+	@GetMapping("studentRecord/{rollNo}")
+	public ResponseEntity getStudentRecord(@PathVariable int rollNo) {
+		try {
+			Optional<StudentRecord> result =studentManagementService.getStudentRecord(rollNo);
+			if(result.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No records found for rollNo "+rollNo);
+			}
+			return ResponseEntity.status(HttpStatus.OK).body(result.get());
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+		}
+	}
+	
+	@GetMapping("studentRecords")
+	public ResponseEntity getStudentRecords() {
+		try {
+			List<StudentRecord> result =studentManagementService.getStudentRecords();
+			if(result.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No records found");
+			}
+			return ResponseEntity.status(HttpStatus.OK).body(result);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+		}
+	}
+	
+	@PutMapping("studentRecord/add")
+	public ResponseEntity addStudentRecord(@RequestBody StudentRecord studentRecord) {
+		try {
+			studentManagementService.addStudentRecord(studentRecord);
+			return ResponseEntity.status(HttpStatus.CREATED).body("Student Records added successfully");
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+		}
+	}
+	
+	@PostMapping("studentRecord/edit/{rollNo}")
+	public ResponseEntity editStudentRecord(@PathVariable int rollNo,@RequestBody StudentRecord studentRecord) {
+		try {
+			boolean result = studentManagementService.editStudentRecord(studentRecord);
+			if(result) {
+				return ResponseEntity.status(HttpStatus.OK).body("Student record updated successfully");
+			}
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student record not found for rollNo "+rollNo);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+		}
+	}
+	
+	@DeleteMapping("studentRecord/delete/{rollNo}")
+	public ResponseEntity deleteStudentRecord(@PathVariable int rollNo) {
+		try {
+			boolean result = studentManagementService.deleteStudentRecord(rollNo);
+			if(result) {
+				return ResponseEntity.status(HttpStatus.OK).body("Student record deleted successfully");
+			}
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student record not found for rollNo "+rollNo);
+		}catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+		}
+	}
+}
